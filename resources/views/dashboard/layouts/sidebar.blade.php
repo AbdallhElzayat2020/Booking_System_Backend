13a2942cc9fcf95b73f3def5d7bc327fe957963e
Abdallh ELzayat <abdallhelzayat.dev@gmail.com>
2026-07-14 06:53:51 +0300

    Lesson 09 - Add locations CRUD using Yajra DataTables

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -14,11 +14,12 @@
 
             {{--  Sections Route  --}}
             <li class="menu-header">Sections</li>
-            <li class="dropdown {{setSidebarActive(['admin.hero.*'])}}">
+            <li class="dropdown {{setSidebarActive(['admin.hero.*','admin.categories.*','admin.locations.*'])}}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Sections</span></a>
                 <ul class="dropdown-menu">
-                    <li class="{{setSidebarActive(['admin.hero.index'])}}"><a class="nav-link" href="{{ route('admin.hero.index') }}">Hero</a></li>
-                    <li class="{{setSidebarActive(['admin.categories.index'])}}"><a class="nav-link" href="{{ route('admin.categories.index') }}">Categories</a></li>
+                    <li class="{{setSidebarActive(['admin.hero.*'])}}"><a class="nav-link" href="{{ route('admin.hero.index') }}">Hero</a></li>
+                    <li class="{{setSidebarActive(['admin.categories.*'])}}"><a class="nav-link" href="{{ route('admin.categories.index') }}">Categories</a></li>
+                    <li class="{{setSidebarActive(['admin.locations.*'])}}"><a class="nav-link" href="{{ route('admin.locations.index') }}">Locations</a></li>
                 </ul>
             </li>
             <li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
